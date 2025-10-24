acfb88a211bca72258fc7745a0ffca8c8fc6a924
kpp39 <kpp39@ya.ru>
2025-10-24 22:32:56 +0300

    forget to remove define that block build

--- include/WebServer.h
+++ include/WebServer.h
@@ -1,5 +1,3 @@
-#ifndef WEBSERVER_H
-#define WEBSERVER_H
 
 #include "Scale.h"
 #include "FlowRate.h"
@@ -12,5 +10,3 @@ extern float calibrationFactor;
 void setupWebServer(Scale &scale, FlowRate &flowRate, BluetoothScale &bluetoothScale, Display &display, BatteryMonitor &battery);
 void startWebServer();
 void stopWebServer();
-
-#endif
